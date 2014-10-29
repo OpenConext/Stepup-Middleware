877b1a9488098fbdcd50c34916a88dea12d18a8b
Reinier Kip <reinier@ibuildings.nl>
2014-10-29 11:08:05 +0100

    Use CommandParamConverter to pass command objects to controller.

---
+++ src/ApiBundle/Controller/CommandController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\ApiBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CommandController extends Controller
+{
+    /**
+     * @ParamConverter(name="command", converter="surfnet_stepup_middleware_api.command")
+     * @param Request $request
+     * @param object $command
+     * @return Response
+     */
+    public function handleAction(Request $request, $command)
+    {
+        $serverName = $request->server->get('SERVER_NAME') ?: $request->server->get('SERVER_ADDR');
+        $response = new JsonResponse(['command' => $command->__uuid, 'processed_by' => $serverName], 202);
+
+        return $response;
+    }
+}

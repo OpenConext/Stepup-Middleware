f08c37eb3749fe482ffe158b2ef6d003e063caa3
Reinier Kip <reinier@ibuildings.nl>
2014-11-12 10:02:12 +0100

    Remove unused use statement.

--- src/Surfnet/Stepup/Identity/Event/YubikeySecondFactorVerified.php
+++ src/Surfnet/Stepup/Identity/Event/YubikeySecondFactorVerified.php
@@ -18,7 +18,6 @@
 
 namespace Surfnet\Stepup\Identity\Event;
 
-use Surfnet\Stepup\Exception\InvalidArgumentException;
 use Surfnet\Stepup\Identity\Value\IdentityId;
 use Surfnet\Stepup\Identity\Value\SecondFactorId;
 use Surfnet\Stepup\Identity\Value\YubikeyPublicId;

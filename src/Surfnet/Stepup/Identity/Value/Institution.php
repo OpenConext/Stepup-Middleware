ec6af60267532430d1ec38de54e4489d92fa292c
DRvanR <dvrenterghem@ibuildings.nl>
2014-11-13 18:17:16 +0100

    Add Institution Value Object and Param Converter

---
+++ src/Surfnet/Stepup/Identity/Value/Institution.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\Stepup\Identity\Value;
+
+use Surfnet\Stepup\Exception\InvalidArgumentException;
+
+class Institution
+{
+    /**
+     * @var string
+     */
+    private $institution;
+
+    /**
+     * @param string $institution may not be an empty string
+     */
+    public function __construct($institution)
+    {
+        if (!is_string($institution) || $institution === '') {
+            throw InvalidArgumentException::invalidType('non-empty string', 'institution', $institution);
+        }
+
+        $this->institution = $institution;
+    }
+
+    /**
+     * @param Institution $otherInstitution
+     * @return bool
+     */
+    public function equals(Institution $otherInstitution)
+    {
+        return $this->institution === $otherInstitution->institution;
+    }
+
+    public function __toString()
+    {
+        return $this->institution;
+    }
+}

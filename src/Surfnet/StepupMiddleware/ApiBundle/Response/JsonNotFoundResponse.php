9ce1e6808184f388a8fd2bf07caed6c895d5a059
DRvanR <dvrenterghem@ibuildings.nl>
2014-12-08 15:50:00 +0100

    Add SRAA api

---
+++ src/Surfnet/StepupMiddleware/ApiBundle/Response/JsonNotFoundResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\ApiBundle\Response;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class JsonNotFoundResponse extends JsonResponse
+{
+    /**
+     * @param array $headers
+     */
+    public function __construct($headers = array())
+    {
+        // using an empty string rather than null, null is converted to an ArrayObject (thus [] in the responsebody)
+        // in the constructor of the json response :(
+        parent::__construct('', 404, $headers);
+    }
+}

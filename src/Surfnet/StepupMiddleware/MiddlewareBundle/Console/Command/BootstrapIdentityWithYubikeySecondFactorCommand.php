224f87d81283889c80880507a0badb548eca53cf
DRvanR <dvrenterghem@ibuildings.nl>
2015-03-18 11:02:41 +0100

    Remove connections from command

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/BootstrapIdentityWithYubikeySecondFactorCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/BootstrapIdentityWithYubikeySecondFactorCommand.php
@@ -26,6 +26,7 @@ use Surfnet\StepupMiddleware\CommandHandlingBundle\Identity\Command\BootstrapIde
     as BootstrapIdentityWithYubikeySecondFactorIdentityCommand;
 use Surfnet\StepupMiddleware\CommandHandlingBundle\Pipeline\Pipeline;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -42,45 +43,29 @@ final class BootstrapIdentityWithYubikeySecondFactorCommand extends Command
      */
     private $eventBus;
 
-    /**
-     * @var Connection
-     */
-    private $middlewareConnection;
-
-    /**
-     * @var Connection
-     */
-    private $gatewayConnection;
-
-    public function __construct(
-        Pipeline $pipeline,
-        BufferedEventBus $eventBus,
-        Connection $middlewareConnection,
-        Connection $gatewayConnection
-    ) {
+    public function __construct(Pipeline $pipeline, BufferedEventBus $eventBus)
+    {
         parent::__construct(null);
 
         $this->pipeline = $pipeline;
         $this->eventBus = $eventBus;
-        $this->middlewareConnection = $middlewareConnection;
-        $this->gatewayConnection = $gatewayConnection;
     }
 
 
     protected function configure()
     {
-        $this->setName('middleware:bootstrap:identity-with-yubikey');
-        $this->setDescription('Creates an identity with a vetted Yubikey second factor');
-        $this->addOption('name-id', null, InputOption::VALUE_REQUIRED, 'The NameID of the identity to create');
-        $this->addOption('institution', null, InputOption::VALUE_REQUIRED, 'The institution of the identity to create');
-        $this->addOption('common-name', null, InputOption::VALUE_REQUIRED, 'The Common Name of the identity to create');
-        $this->addOption('email', null, InputOption::VALUE_REQUIRED, 'The e-mail address of the identity to create');
-        $this->addOption(
-            'yubikey',
-            null,
-            InputOption::VALUE_REQUIRED,
-            'The public ID of the Yubikey. Remove the last 32 characters of a Yubikey OTP to acquire this.'
-        );
+        $this
+            ->setName('middleware:bootstrap:identity-with-yubikey')
+            ->setDescription('Creates an identity with a vetted Yubikey second factor')
+            ->addArgument('name-id', InputArgument::REQUIRED, 'The NameID of the identity to create')
+            ->addArgument('institution', InputArgument::REQUIRED, 'The institution of the identity to create')
+            ->addArgument('common-name', InputArgument::REQUIRED, 'The Common Name of the identity to create')
+            ->addArgument('email', InputArgument::REQUIRED, 'The e-mail address of the identity to create')
+            ->addArgument(
+                'yubikey',
+                InputArgument::REQUIRED,
+                'The public ID of the Yubikey. Remove the last 32 characters of a Yubikey OTP to acquire this.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -88,30 +73,23 @@ final class BootstrapIdentityWithYubikeySecondFactorCommand extends Command
         $command = new BootstrapIdentityWithYubikeySecondFactorIdentityCommand();
         $command->UUID = (string) Uuid::uuid4();
         $command->identityId = (string) Uuid::uuid4();
-        $command->nameId = $input->getOption('name-id');
-        $command->institution = $input->getOption('institution');
-        $command->commonName = $input->getOption('common-name');
-        $command->email = $input->getOption('email');
+        $command->nameId = $input->getArgument('name-id');
+        $command->institution = $input->getArgument('institution');
+        $command->commonName = $input->getArgument('common-name');
+        $command->email = $input->getArgument('email');
         $command->secondFactorId = (string) Uuid::uuid4();
-        $command->yubikeyPublicId = $input->getOption('yubikey');
-
-        $this->middlewareConnection->beginTransaction();
-        $this->gatewayConnection->beginTransaction();
+        $command->yubikeyPublicId = $input->getArgument('yubikey');
 
         try {
             $command = $this->pipeline->process($command);
             $this->eventBus->flush();
 
-            $this->gatewayConnection->commit();
-            $this->middlewareConnection->commit();
         } catch (Exception $e) {
             $output->writeln(sprintf(
                 '<error>An Error occurred when trying to bootstrap the identity: "%s"</error>',
                 $e->getMessage()
             ));
 
-            $this->middlewareConnection->rollBack();
-            $this->middlewareConnection->rollBack();
             throw $e;
         }
 

423691d89732d4d529e2a03e48427612a9f220dd
DRvanR <dvrenterghem@ibuildings.nl>
2015-03-23 15:38:53 +0100

    Add EventDispatchingStage to be used in StagedPipeline

---
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Pipeline/EventDispatchingStage.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Pipeline;
+
+use Psr\Log\LoggerInterface;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Command\Command;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\EventHandling\BufferedEventBus;
+
+class EventDispatchingStage implements Stage
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var BufferedEventBus
+     */
+    private $bufferedEventBus;
+
+    public function __construct(BufferedEventBus $bufferedEventBus, LoggerInterface $logger)
+    {
+        $this->bufferedEventBus = $bufferedEventBus;
+        $this->logger = $logger;
+    }
+
+    public function process(Command $command)
+    {
+        $this->logger->debug(sprintf('Dispatching Events for "%s"', $command));
+
+        $this->bufferedEventBus->flush();
+
+        $this->logger->debug(sprintf('All events for "%s" have been dispatched', $command));
+
+        return $command;
+    }
+}
---
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Tests/Pipeline/EventDispatchingStageTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Tests\Pipeline;
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase as UnitTest;
+use Psr\Log\NullLogger;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Pipeline\EventDispatchingStage;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Tests\Command\FixedUuidStubCommand;
+
+class EventDispatchingStageTest extends UnitTest
+{
+    /**
+     * @test
+     * @group pipeline
+     */
+    public function buffered_event_bus_flush_is_called_during_process()
+    {
+        $command = m::mock('Surfnet\StepupMiddleware\CommandHandlingBundle\Command\Command');
+        $eventBus = m::mock('Surfnet\StepupMiddleware\CommandHandlingBundle\EventHandling\BufferedEventBus')
+            ->shouldReceive('flush')->once()
+            ->getMock();
+
+        $stage = new EventDispatchingStage($eventBus, new NullLogger());
+        $stage->process($command);
+    }
+
+    /**
+     * @test
+     * @group pipeline
+     */
+    public function it_returns_the_same_command_as_it_processes_unmodified()
+    {
+        $command = new FixedUuidStubCommand();
+        $uuid = $command->UUID;
+
+        $eventBus = m::mock('Surfnet\StepupMiddleware\CommandHandlingBundle\EventHandling\BufferedEventBus')
+            ->shouldReceive('flush')->once()
+            ->getMock();
+
+        $stage = new EventDispatchingStage($eventBus, new NullLogger());
+        $returnedCommand = $stage->process($command);
+
+        $this->assertEquals(spl_object_hash($command), spl_object_hash($returnedCommand));
+        $this->assertEquals($uuid, $returnedCommand->UUID);
+    }
+}

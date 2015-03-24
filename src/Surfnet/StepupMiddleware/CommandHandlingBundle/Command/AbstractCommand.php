67ee81f1d548102c50b98076484574c4306f0b8e
Reinier Kip <reinier@ibuildings.nl>
2015-03-24 18:33:10 +0100

    Correctly validate command UUIDs.

--- src/Surfnet/StepupMiddleware/CommandHandlingBundle/Command/AbstractCommand.php
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Command/AbstractCommand.php
@@ -25,7 +25,7 @@ abstract class AbstractCommand implements Command
     /**
      * @Assert\NotBlank()
      * @Assert\Type(type="string")
-     * @Assert\Regex(pattern="~^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$~")
+     * @Assert\Regex(pattern="~^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$~i")
      *
      * @var string
      */

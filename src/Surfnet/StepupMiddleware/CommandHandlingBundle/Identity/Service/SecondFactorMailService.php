d9c1440cd5ef6c5e062b7f5f56ea7a02886b839b
Reinier Kip <reinier@ibuildings.nl>
2015-03-25 12:09:51 +0100

    Render e-mails from string.

--- src/Surfnet/StepupMiddleware/CommandHandlingBundle/Identity/Service/SecondFactorMailService.php
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Identity/Service/SecondFactorMailService.php
@@ -87,7 +87,9 @@ class SecondFactorMailService
     ) {
         $subject = $this->translator->trans(
             'ss.mail.email_verification_email.subject',
-            ['%commonName%' => $commonName]
+            ['%commonName%' => $commonName],
+            null,
+            $locale
         );
 
         $verificationUrl = str_replace(
@@ -96,16 +98,15 @@ class SecondFactorMailService
             $this->emailVerificationUrlTemplate
         );
         $parameters = [
+            'templateString'   => self::VERIFICATION_TEMPLATE,
             'locale'           => $locale,
             'commonName'       => $commonName,
             'email'            => $email,
             'verificationUrl'  => $verificationUrl
         ];
 
-        // Rendering file template instead of string
-        // (https://github.com/symfony/symfony/issues/10865#issuecomment-42438248)
         $body = $this->templateEngine->render(
-            'SurfnetStepupMiddlewareCommandHandlingBundle:SecondFactorMailService:emailVerificationEmail.html.twig',
+            'SurfnetStepupMiddlewareCommandHandlingBundle:SecondFactorMailService:email.html.twig',
             $parameters
         );
 
@@ -136,10 +137,13 @@ class SecondFactorMailService
     ) {
         $subject = $this->translator->trans(
             'ss.mail.registration_email.subject',
-            ['%commonName%' => $commonName]
+            ['%commonName%' => $commonName],
+            null,
+            $locale
         );
 
         $parameters = [
+            'templateString'   => self::REGISTRATION_TEMPLATE,
             'locale'           => $locale,
             'commonName'       => $commonName,
             'email'            => $email,
@@ -150,7 +154,7 @@ class SecondFactorMailService
         // Rendering file template instead of string
         // (https://github.com/symfony/symfony/issues/10865#issuecomment-42438248)
         $body = $this->templateEngine->render(
-            'SurfnetStepupMiddlewareCommandHandlingBundle:SecondFactorMailService:registrationEmail.html.twig',
+            'SurfnetStepupMiddlewareCommandHandlingBundle:SecondFactorMailService:email.html.twig',
             $parameters
         );
 
@@ -164,4 +168,94 @@ class SecondFactorMailService
 
         $this->mailer->send($message);
     }
+
+    const VERIFICATION_TEMPLATE = <<<TWIG
+<p style="font-style:italic;text-align:center">- English version of this message below-</p>
+
+<p>Beste {{ commonName }},</p>
+
+<p>Bedankt voor het registreren van je token. Klik op onderstaande link om je e-mailadres te bevestigen:</p>
+<p><a href="{{ verificationUrl }}">{{ verificationUrl }}</a></p>
+<p>Is klikken op de link niet mogelijk? Kopieer dan de link en plak deze in de adresbalk van je browser.</p>
+<p>SURFnet</p>
+
+<hr>
+
+<p>Dear {{ commonName }},</p>
+
+<p>Thank you for registering your token. Please visit this link to verify your email address:</p>
+<p><a href="{{ verificationUrl }}">{{ verificationUrl }}</a></p>
+<p>If you can not click on the URL, please copy the link and paste it in the address bar of your browser.</p>
+<p>SURFnet</p>
+TWIG;
+
+    const REGISTRATION_TEMPLATE = <<<TWIG
+<p style="font-style:italic;text-align:center">- English version of this message below-</p>
+
+<p>Beste {{ commonName }},</p>
+
+<p>Bedankt voor het registreren van je token. Je token is bijna klaar voor gebruik. Ga naar de Service Desk om je token te laten activeren. </p>
+<p>Neem aub het volgende mee:</p>
+<ul>
+    <li>Je token</li>
+    <li>Een geldig legitimatiebewijs (paspoort, rijbewijs of nationale ID-kaart)</li>
+    <li>De registratiecode uit deze e-mail</li>
+</ul>
+
+<p style="font-size: 150%; text-align: center">
+    <code>{{ registrationCode }}</code>
+</p>
+
+<p>Service Desk medewerkers die je token kunnen activeren:</p>
+
+{% if ras is empty %}
+    <p>Er zijn geen Service Desk medewerkers beschikbaar.</p>
+{% else %}
+    <ul>
+        {% for ra in ras %}
+            <li>
+                <address>
+                    <strong>{{ ra.commonName }}</strong><br>
+                    {{ ra.location }}<br>
+                    {{ ra.contactInformation }}
+                </address>
+            </li>
+        {% endfor %}
+    </ul>
+{% endif %}
+
+<hr>
+
+<p>Dear {{ commonName }},</p>
+
+<p>Thank you for registering your token, you are almost ready now. Please visit the Service Desk to activate your token.</p>
+<p>Please bring the following:</p>
+<ul>
+    <li>Your token</li>
+    <li>A valid identity document (passport, drivers license or national ID-card.</li>
+    <li>The registration code from this e-mail</li>
+</ul>
+
+<p style="font-size: 150%; text-align: center">
+    <code>{{ registrationCode }}</code>
+</p>
+
+<p>Service Desk employees authorized to activate your token:</p>
+
+{% if ras is empty %}
+    <p>No Service Desk employees are available.</p>
+{% else %}
+    <ul>
+        {% for ra in ras %}
+            <li>
+                <address>
+                    <strong>{{ ra.commonName }}</strong><br>
+                    {{ ra.location }}<br>
+                    {{ ra.contactInformation }}
+                </address>
+            </li>
+        {% endfor %}
+    </ul>
+{% endif %}
+TWIG;
 }

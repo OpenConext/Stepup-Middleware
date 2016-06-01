de2b0b29a7dfd9e1f4c5d0b0ea33b9ce407f0656
Boy Baukema <boy@ibuildings.nl>
2016-06-01 12:21:44 +0200

    Remove accidentally comitted local files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -54,8 +54,6 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-
-
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');

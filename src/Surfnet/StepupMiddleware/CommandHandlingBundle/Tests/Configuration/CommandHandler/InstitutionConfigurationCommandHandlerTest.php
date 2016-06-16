18f7e440bc770aec4a6255add890ae725ddd002e
Alex Rothuis <arothuis@ibuildings.nl>
2016-06-16 15:46:38 +0200

    Add AddRaLocation command, method in handler, test

---
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Tests/Configuration/CommandHandler/InstitutionConfigurationCommandHandlerTest.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Copyright 2016 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Tests\Configuration\CommandHandler;
+
+use Broadway\CommandHandling\CommandHandlerInterface;
+use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventSourcing\AggregateFactory\PublicConstructorAggregateFactory;
+use Broadway\EventStore\EventStoreInterface;
+use Surfnet\Stepup\Configuration\Event\NewInstitutionConfigurationCreatedEvent;
+use Surfnet\Stepup\Configuration\Event\RaLocationAddedEvent;
+use Surfnet\Stepup\Configuration\EventSourcing\InstitutionConfigurationRepository;
+use Surfnet\Stepup\Configuration\Value\Institution;
+use Surfnet\Stepup\Configuration\Value\InstitutionConfigurationId;
+use Surfnet\Stepup\Configuration\Value\RaLocationId;
+use Surfnet\Stepup\Configuration\Value\RaLocationName;
+use Surfnet\Stepup\Configuration\Value\ContactInformation;
+use Surfnet\Stepup\Configuration\Value\Location;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Configuration\Command\AddRaLocationCommand;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Configuration\CommandHandler\InstitutionConfigurationCommandHandler;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Tests\CommandHandlerTest;
+
+class InstitutionConfigurationCommandHandlerTest extends CommandHandlerTest
+{
+    /**
+     * @test
+     * @group command-handler
+     */
+    public function an_ra_location_can_be_added_to_an_existing_institution_configuration()
+    {
+        $command                     = new AddRaLocationCommand();
+        $command->raLocationId       = self::uuid();
+        $command->institution        = 'An institution';
+        $command->raLocationName     = 'An RA location name';
+        $command->location           = 'A location';
+        $command->contactInformation = 'Some contact information';
+
+        $institution                = new Institution($command->institution);
+        $institutionConfigurationId = InstitutionConfigurationId::from($institution);
+
+        $this->scenario
+            ->withAggregateId($institutionConfigurationId)
+            ->given([
+                new NewInstitutionConfigurationCreatedEvent(
+                    $institutionConfigurationId,
+                    $institution
+                ),
+            ])
+            ->when($command)
+            ->then([
+                new RaLocationAddedEvent(
+                    $institutionConfigurationId,
+                    new RaLocationId($command->raLocationId),
+                    new RaLocationName($command->raLocationName),
+                    new Location($command->location),
+                    new ContactInformation($command->contactInformation)
+                )
+            ]);
+    }
+
+    /**
+     * @test
+     * @group command-handler
+     */
+    public function an_ra_location_is_added_to_a_newly_created_an_institution_configuration_is_created_when_there_is_none()
+    {
+        $command                     = new AddRaLocationCommand();
+        $command->raLocationId       = self::uuid();
+        $command->institution        = 'An institution';
+        $command->raLocationName     = 'An RA location name';
+        $command->location           = 'A location';
+        $command->contactInformation = 'Some contact information';
+
+        $institution                = new Institution($command->institution);
+        $institutionConfigurationId = InstitutionConfigurationId::from($institution);
+
+        $this->scenario
+            ->withAggregateId($institutionConfigurationId)
+            ->when($command)
+            ->then([
+                new NewInstitutionConfigurationCreatedEvent(
+                    $institutionConfigurationId,
+                    $institution
+                ),
+                new RaLocationAddedEvent(
+                    $institutionConfigurationId,
+                    new RaLocationId($command->raLocationId),
+                    new RaLocationName($command->raLocationName),
+                    new Location($command->location),
+                    new ContactInformation($command->contactInformation)
+                )
+            ]);
+    }
+
+    /**
+     * @test
+     * @group command-handler
+     */
+    public function the_same_ra_location_cannot_be_added_twice()
+    {
+        $this->setExpectedException('Surfnet\Stepup\Exception\DomainException', 'already present');
+
+        $command                     = new AddRaLocationCommand();
+        $command->raLocationId       = self::uuid();
+        $command->institution        = 'An institution';
+        $command->raLocationName     = 'An RA location name';
+        $command->location           = 'A location';
+        $command->contactInformation = 'Some contact information';
+
+        $institution                = new Institution($command->institution);
+        $institutionConfigurationId = InstitutionConfigurationId::from($institution);
+
+        $this->scenario
+            ->withAggregateId($institutionConfigurationId)
+            ->given([
+                new NewInstitutionConfigurationCreatedEvent(
+                    $institutionConfigurationId,
+                    $institution
+                ),
+                new RaLocationAddedEvent(
+                    $institutionConfigurationId,
+                    new RaLocationId($command->raLocationId),
+                    new RaLocationName($command->raLocationName),
+                    new Location($command->location),
+                    new ContactInformation($command->contactInformation)
+                )
+            ])
+            ->when($command);
+    }
+
+    /**
+     * Create a command handler for the given scenario test case.
+     *
+     * @param EventStoreInterface $eventStore
+     * @param EventBusInterface $eventBus
+     *
+     * @return CommandHandlerInterface
+     */
+    protected function createCommandHandler(EventStoreInterface $eventStore, EventBusInterface $eventBus)
+    {
+        $aggregateFactory = new PublicConstructorAggregateFactory();
+
+        return new InstitutionConfigurationCommandHandler(
+            new InstitutionConfigurationRepository($eventStore, $eventBus, $aggregateFactory)
+        );
+    }
+}

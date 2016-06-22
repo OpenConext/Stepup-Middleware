05ff786f9c07742cdcab4e587356a5f03f5990d5
Alex Rothuis <arothuis@ibuildings.nl>
2016-06-22 17:39:47 +0200

    Correct migration (again)

--- app/DoctrineMigrations/Version20160622160146.php
+++ app/DoctrineMigrations/Version20160622160146.php
@@ -30,7 +30,7 @@ class Version20160622160146 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE institution_with_ra_locations (institution VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(institution)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE institution_with_personal_ra_details');
+        $this->addSql('CREATE TABLE institution_with_personal_ra_details (institution VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(institution)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE institution_with_ra_locations');
     }
 }

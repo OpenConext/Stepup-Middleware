27c90ddbd5be6db70e03d54280b4e80544ca6e9c
Alex Rothuis <arothuis@ibuildings.nl>
2016-07-12 15:04:59 +0200

    Introduce event listener to test nested events in buffered event bus

---
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Tests/EventHandling/RecordEventsAndPublishToBusOnFirstCallEventListener.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Copyright 2016 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Tests\EventHandling;
+
+use Broadway\Domain\DomainEventStream;
+use Broadway\Domain\DomainMessage;
+use Broadway\EventHandling\EventListenerInterface;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\EventHandling\BufferedEventBus;
+
+class RecordEventsAndPublishToBusOnFirstCallEventListener implements EventListenerInterface
+{
+    /**
+     * @var integer
+     */
+    private $callCount = 0;
+
+    /**
+     * @var BufferedEventBus
+     */
+    private $eventBus;
+
+    /**
+     * @var DomainEventStream
+     */
+    private $toPublish;
+
+    /**
+     * @var DomainMessage[]
+     */
+    private $recordedEvents = [];
+
+    /**
+     * @param BufferedEventBus $eventBus
+     * @param DomainEventStream $toPublish
+     */
+    public function __construct(BufferedEventBus $eventBus, DomainEventStream $toPublish)
+    {
+        $this->eventBus  = $eventBus;
+        $this->toPublish = $toPublish;
+    }
+
+    /**
+     * @param DomainMessage $domainMessage
+     */
+    public function handle(DomainMessage $domainMessage)
+    {
+        $this->recordedEvents[] = $domainMessage;
+
+        if ($this->callCount === 0) {
+            $this->eventBus->publish($this->toPublish);
+            $this->callCount = 1;
+        }
+    }
+
+    /**
+     * @return DomainMessage[]
+     */
+    public function getRecordedEvents()
+    {
+        return $this->recordedEvents;
+    }
+}

c79f393ced85b989ced6c0351137194c7491f4b0
Alex Rothuis <arothuis@ibuildings.nl>
2016-07-13 13:56:49 +0200

    Introduce ConfiguredInstitutionProjector

---
+++ src/Surfnet/StepupMiddleware/ApiBundle/Configuration/Projector/ConfiguredInstitutionProjector.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright 2016 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\ApiBundle\Configuration\Projector;
+
+use Broadway\ReadModel\Projector;
+use Surfnet\Stepup\Configuration\Event\NewInstitutionConfigurationCreatedEvent;
+use Surfnet\StepupMiddleware\ApiBundle\Configuration\Entity\ConfiguredInstitution;
+use Surfnet\StepupMiddleware\ApiBundle\Configuration\Repository\ConfiguredInstitutionRepository;
+
+final class ConfiguredInstitutionProjector extends Projector
+{
+    /**
+     * @var ConfiguredInstitutionRepository
+     */
+    private $configuredInstitutionRepository;
+
+    public function __construct(ConfiguredInstitutionRepository $configuredInstitutionRepository)
+    {
+        $this->configuredInstitutionRepository = $configuredInstitutionRepository;
+    }
+
+    public function applyNewInstitutionConfigurationCreatedEvent(NewInstitutionConfigurationCreatedEvent $event)
+    {
+        $this->configuredInstitutionRepository->save(ConfiguredInstitution::createFrom($event->institution));
+    }
+}

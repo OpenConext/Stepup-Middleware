de1a2a1b5620c2f7a526dfed9177764bfb9acaf7
Alex Rothuis <arothuis@ibuildings.nl>
2016-07-25 09:48:29 +0200

    Import Assert so command can be validated properly

--- src/Surfnet/StepupMiddleware/CommandHandlingBundle/Configuration/Command/ConfigureInstitutionConfigurationOptionsCommand.php
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Configuration/Command/ConfigureInstitutionConfigurationOptionsCommand.php
@@ -18,6 +18,8 @@
 
 namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Configuration\Command;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class ConfigureInstitutionConfigurationOptionsCommand
 {
     /**

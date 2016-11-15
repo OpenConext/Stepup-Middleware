f1284f60aa0e7a5169087d48b74fd9962b152e87
Alex Rothuis <arothuis@ibuildings.nl>
2016-11-15 18:21:22 +0100

    Register separate mail services

--- src/Surfnet/StepupMiddleware/CommandHandlingBundle/DependencyInjection/SurfnetStepupMiddlewareCommandHandlingExtension.php
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/DependencyInjection/SurfnetStepupMiddlewareCommandHandlingExtension.php
@@ -50,5 +50,23 @@ class SurfnetStepupMiddlewareCommandHandlingExtension extends Extension
             ->replaceArgument(4, $config['self_service_email_verification_url_template'])
             ->replaceArgument(6, $config['email_fallback_locale'])
             ->replaceArgument(7, $config['self_service_url']);
+
+        $container
+            ->getDefinition('surfnet_stepup_middleware_command_handling.service.email_verification_mail')
+            ->replaceArgument(4, $config['self_service_email_verification_url_template'])
+            ->replaceArgument(6, $config['email_fallback_locale']);
+
+        $container
+            ->getDefinition('surfnet_stepup_middleware_command_handling.service.registration_mail')
+            ->replaceArgument(5, $config['email_fallback_locale']);
+
+        $container
+            ->getDefinition('surfnet_stepup_middleware_command_handling.service.second_factor_revocation_mail')
+            ->replaceArgument(5, $config['email_fallback_locale'])
+            ->replaceArgument(6, $config['self_service_url']);
+
+        $container
+            ->getDefinition('surfnet_stepup_middleware_command_handling.service.registration_mail')
+            ->replaceArgument(5, $config['email_fallback_locale']);
     }
 }

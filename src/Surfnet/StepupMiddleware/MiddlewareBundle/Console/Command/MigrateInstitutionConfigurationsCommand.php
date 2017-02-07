0ac622f0baa0e961361dbe3a96ef0b96b082676b
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-07 12:11:59 +0100

    Implement InstitutionConfiguration migration
    The InstitutionConfigurations are migrated in order to update existing InstitutionConfigurationIds to be handled case-insensitively.

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/MigrateInstitutionConfigurationsCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/MigrateInstitutionConfigurationsCommand.php
@@ -18,9 +18,14 @@
 
 namespace Surfnet\StepupMiddleware\MiddlewareBundle\Console\Command;
 
+use Exception;
+use Sensio\Bundle\GeneratorBundle\Command\Helper\QuestionHelper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 
 final class MigrateInstitutionConfigurationsCommand extends Command
 {
@@ -35,5 +40,74 @@ final class MigrateInstitutionConfigurationsCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var QuestionHelper $questionHelper */
+        $questionHelper = $this->getHelper('question');
+
+        $output->writeln([
+            '<error>WARNING: you are about to migrate institution configurations'
+            . ' to work with identifiers based on insitutions with normalized casing.</error>',
+            '<error>This command is intended to be run only once.</error>',
+            ''
+        ]);
+
+        $confirmationQuestion = new ConfirmationQuestion(
+            'Are you sure you want to run the institution configuration migrations? [y/N]',
+            false
+        );
+        $confirmed = $questionHelper->ask($input, $output, $confirmationQuestion);
+
+        if (!$confirmed) {
+            $output->writeln('<info>Exiting without running migrations.</info>');
+            return;
+        }
+
+        /** @var Container $container */
+        $container = $this->getApplication()->getKernel()->getContainer();
+
+        $tokenStorage     = $container->get('security.token_storage');
+        $connectionHelper = $container->get('surfnet_stepup_middleware_middleware.dbal_connection_helper');
+        $provider         = $container->get('surfnet_stepup_middleware_middleware.institution_configuration_provider');
+        $pipeline         = $container->get('pipeline');
+        $eventBus         = $container->get('surfnet_stepup_middleware_command_handling.event_bus.buffered');
+
+        // The InstitutionConfiguration commands require ROLE_MANAGEMENT
+        // Note that the new events will not have any actor metadata associated with them
+        $tokenStorage->setToken(new AnonymousToken('console', 'console', ['ROLE_MANAGEMENT']));
+
+        $connectionHelper->beginTransaction();
+
+        try {
+            $state = $provider->loadData();
+
+            foreach ($state->inferRemovalCommands() as $removalCommand) {
+                $pipeline->process($removalCommand);
+            }
+            $eventBus->flush();
+
+            foreach ($state->inferCreateCommands() as $createCommand) {
+                $pipeline->process($createCommand);
+            }
+            $eventBus->flush();
+
+            foreach ($state->inferReconfigureCommands() as $reconfigureCommand) {
+                $pipeline->process($reconfigureCommand);
+            }
+            $eventBus->flush();
+
+            foreach ($state->inferAddRaLocationCommands() as $addRaLocationCommand) {
+                $pipeline->process($addRaLocationCommand);
+            }
+            $eventBus->flush();
+
+            $connectionHelper->commit();
+            $output->writeln('<info>Successfully migrated institution configurations.</info>');
+        } catch (Exception $exception) {
+            $output->writeln(
+                sprintf('<error>Could not migrate institution configurations: %s</error>', $exception->getMessage())
+            );
+            $connectionHelper->rollBack();
+
+            throw $exception;
+        }
     }
 }

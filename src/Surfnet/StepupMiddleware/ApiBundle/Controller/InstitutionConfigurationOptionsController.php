21a124132f48e29cb14d2170263c89a4821aabe7
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-10 17:11:24 +0100

    Add "allowed_second_factors" to Middleware InstConf API

--- src/Surfnet/StepupMiddleware/ApiBundle/Controller/InstitutionConfigurationOptionsController.php
+++ src/Surfnet/StepupMiddleware/ApiBundle/Controller/InstitutionConfigurationOptionsController.php
@@ -26,21 +26,32 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class InstitutionConfigurationOptionsController extends Controller
 {
-    public function getForInstitutionAction($institution)
+    public function getForInstitutionAction($institutionValue)
     {
         $this->denyAccessUnlessGranted(['ROLE_SS', 'ROLE_RA']);
 
+        $institution = new Institution($institutionValue);
+
         $institutionConfigurationOptions = $this
             ->getInstitutionConfigurationOptionsService()
-            ->findInstitutionConfigurationOptionsFor(new Institution($institution));
+            ->findInstitutionConfigurationOptionsFor($institution);
+
+        $allowedSecondFactorList = $this
+            ->getAllowedSecondFactorListService()
+            ->getAllowedSecondFactorListFor($institution);
 
         if ($institutionConfigurationOptions === null) {
             throw new NotFoundHttpException(
-                sprintf('No institution configuration options found for institution "%s"', $institution)
+                sprintf('No institution configuration options found for institution "%s"', $institutionValue)
             );
         }
 
-        return new JsonResponse($institutionConfigurationOptions);
+        return new JsonResponse([
+            'institution'                  => $institutionConfigurationOptions->institution,
+            'use_ra_locations'             => $institutionConfigurationOptions->useRaLocationsOption,
+            'show_raa_contact_information' => $institutionConfigurationOptions->showRaaContactInformationOption,
+            'allowed_second_factors'       => $allowedSecondFactorList
+        ]);
     }
 
     /**
@@ -49,5 +60,13 @@ final class InstitutionConfigurationOptionsController extends Controller
     private function getInstitutionConfigurationOptionsService()
     {
         return $this->get('surfnet_stepup_middleware_api.service.institution_configuration_options');
+    }
+
+    /**
+     * @return object|\Surfnet\StepupMiddleware\ApiBundle\Configuration\Service\AllowedSecondFactorListService
+     */
+    private function getAllowedSecondFactorListService()
+    {
+        return $this->get('surfnet_stepup_middleware_api.service.allowed_second_factor_list');
     }
 }

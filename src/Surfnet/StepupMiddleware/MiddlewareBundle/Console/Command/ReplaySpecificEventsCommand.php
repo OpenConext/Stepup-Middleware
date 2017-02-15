8b0b60f8d2beae85210c00b38dfd4a208d58f034
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-15 16:31:49 +0100

    Add projection overview to ReplaySpecificEventsCommand

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
@@ -18,6 +18,7 @@
 
 namespace Surfnet\StepupMiddleware\MiddlewareBundle\Console\Command;
 
+use Broadway\ReadModel\ProjectorInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,6 +29,7 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 class ReplaySpecificEventsCommand extends ContainerAwareCommand
 {
     const OPTION_LIST_EVENTS = 'list-events';
+    const OPTION_LIST_PROJECTORS = 'list-projectors';
 
     protected function configure()
     {
@@ -39,19 +41,40 @@ class ReplaySpecificEventsCommand extends ContainerAwareCommand
                 null,
                 InputOption::VALUE_NONE,
                 'List all events available to replay'
+            )
+            ->addOption(
+                self::OPTION_LIST_PROJECTORS,
+                null,
+                InputOption::VALUE_NONE,
+                'List all projectors available to use when replaying events'
             );
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getContainer();
-        $eventCollection = $container->get('middleware.event_replay.event_collection');
+        $eventCollection     = $container->get('middleware.event_replay.event_collection');
+        $projectorCollection = $container->get('middleware.event_replay.projector_collection');
 
         if ($input->getOption(self::OPTION_LIST_EVENTS)) {
             $availableEvents = iterator_to_array($eventCollection);
 
             $output->writeln('<info>The following events can be replayed:</info>');
             $output->writeln(!empty($availableEvents) ? $availableEvents : 'None.');
+
+            return;
+        }
+
+        if ($input->getOption(self::OPTION_LIST_PROJECTORS)) {
+            $availableProjectors = array_map(
+                function (ProjectorInterface $projector) {
+                    return get_class($projector);
+                },
+                iterator_to_array($projectorCollection)
+            );
+
+            $output->writeln('<info>The following projectors can be used when replaying events:</info>');
+            $output->writeln(!empty($availableProjectors) ? $availableProjectors : 'None.');
 
             return;
         }

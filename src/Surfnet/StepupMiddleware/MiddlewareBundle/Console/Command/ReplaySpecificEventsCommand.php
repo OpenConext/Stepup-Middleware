1aeaf0ee26868af156fb056e9d9781b545c7c75e
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-15 16:12:42 +0100

    Add option to list all events available to replay

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
@@ -21,22 +21,38 @@ namespace Surfnet\StepupMiddleware\MiddlewareBundle\Console\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class ReplaySpecificEventsCommand extends ContainerAwareCommand
 {
+    const OPTION_LIST_EVENTS = 'list-events';
+
     protected function configure()
     {
         $this
             ->setName('stepup:event:replay')
-            ->setDescription('replay specified events for specified projectors');
+            ->setDescription('replay specified events for specified projectors')
+            ->addOption(
+                self::OPTION_LIST_EVENTS,
+                null,
+                InputOption::VALUE_NONE,
+                'List all events available to replay'
+            );
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getContainer();
         $eventCollection = $container->get('middleware.event_replay.event_collection');
+
+        if ($input->getOption(self::OPTION_LIST_EVENTS)) {
+            $output->writeln('<info>The following events can be replayed:</info>');
+            $output->writeln(iterator_to_array($eventCollection));
+
+            return;
+        }
 
         /** @var QuestionHelper $questionHelper */
         $questionHelper = $this->getHelper('question');

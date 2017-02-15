4cf3dcce1b3c2fb0e7060192751fa643823c80f4
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-15 15:51:08 +0100

    Make ReplaySpecificEventsCommand container aware

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
@@ -22,7 +22,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ReplaySpecificEventsCommand extends Command
+class ReplaySpecificEventsCommand extends ContainerAwareCommand
 {
     protected function configure()
     {

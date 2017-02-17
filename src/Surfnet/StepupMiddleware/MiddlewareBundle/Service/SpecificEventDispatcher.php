e5192c9136b8bff8b4d4f0845539c31f21e99735
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-17 10:23:33 +0100

    Introduce specific event dispatcher
    The event dispatcher will be able to dispatch a selection of events for a selection of projectors.

---
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Service/SpecificEventDispatcher.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\MiddlewareBundle\Service;
+
+use Exception;
+use Surfnet\StepupMiddleware\MiddlewareBundle\EventSourcing\DBALEventHydrator;
+use Surfnet\StepupMiddleware\MiddlewareBundle\EventSourcing\EventCollection;
+use Surfnet\StepupMiddleware\MiddlewareBundle\EventSourcing\ProjectorCollection;
+
+final class SpecificEventDispatcher
+{
+    /**
+     * @var DBALConnectionHelper
+     */
+    private $connectionHelper;
+
+    /**
+     * @var DBALEventHydrator
+     */
+    private $eventHydrator;
+
+    public function __construct(DBALConnectionHelper $connectionHelper, DBALEventHydrator $eventHydrator)
+    {
+        $this->connectionHelper = $connectionHelper;
+        $this->eventHydrator = $eventHydrator;
+    }
+
+    public function dispatchEventsForProjectors(
+        EventCollection $eventCollection,
+        ProjectorCollection $projectorCollection
+    ) {
+        $this->connectionHelper->beginTransaction();
+
+        try {
+            // Get hydrated events
+
+            // foreach event; foreach projector; $projector->handle($event)
+
+            $this->connectionHelper->commit();
+        } catch (Exception $exception) {
+            $this->connectionHelper->rollBack();
+        }
+    }
+}

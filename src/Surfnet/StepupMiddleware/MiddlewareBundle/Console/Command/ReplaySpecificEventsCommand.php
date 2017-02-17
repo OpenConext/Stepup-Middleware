9bd709946f76a0ab8213025ac09bda42dc0d15b1
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-17 11:27:19 +0100

    Add dispatching events in ReplaySpecificEventsCommand

--- src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
+++ src/Surfnet/StepupMiddleware/MiddlewareBundle/Console/Command/ReplaySpecificEventsCommand.php
@@ -18,7 +18,6 @@
 
 namespace Surfnet\StepupMiddleware\MiddlewareBundle\Console\Command;
 
-use Broadway\ReadModel\ProjectorInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
@@ -88,8 +87,8 @@ class ReplaySpecificEventsCommand extends ContainerAwareCommand
         );
         $selectEventsQuestion->setMultiselect(true);
 
-        $chosenEvents = $questionHelper->ask($input, $output, $selectEventsQuestion);
-        $eventCollection->select($chosenEvents);
+        $chosenEvents   = $questionHelper->ask($input, $output, $selectEventsQuestion);
+        $eventSelection = $eventCollection->select($chosenEvents);
 
         $selectProjectorsQuestion = new ChoiceQuestion(
             'For which projectors would you like to replay the selected events? '
@@ -98,7 +97,10 @@ class ReplaySpecificEventsCommand extends ContainerAwareCommand
         );
         $selectProjectorsQuestion->setMultiselect(true);
 
-        $chosenProjectors = $questionHelper->ask($input, $output, $selectProjectorsQuestion);
-        $projectorCollection->selectByNames($chosenProjectors);
+        $chosenProjectors   = $questionHelper->ask($input, $output, $selectProjectorsQuestion);
+        $projectorSelection = $projectorCollection->selectByNames($chosenProjectors);
+
+        $eventDispatcher = $container->get('middleware.event_replay.specific_event_dispatcher');
+        $eventDispatcher->dispatchEventsForProjectors($eventSelection, $projectorSelection);
     }
 }

32860504b8ddce109d9bf89c61d3a7452688b41f
Joris Steyn <joris@ibuildings.nl>
2018-03-15 10:17:22 +0100

    Remove differences between develop and master
    These changes were added in 1.6.0 and only on the master branch.

--- update-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -XPOST -v \
-    -H "Accept: application/json" \
-    -H "Content-type: application/json" \
-    -k \
-    -d @config.json \
-    http://management:admin@mw-dev.stepup.coin.surf.net/app_dev.php/management/configuration

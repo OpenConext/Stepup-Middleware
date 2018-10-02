69f111b92710d4cd353b915c8ac2a0cd42a946d4
Bas Strooband <bstrooband@ibuildings.nl>
2018-10-02 17:38:58 +0200

    Add test for InsitutionRoleTest
    A test was missing and is added now

---
+++ src/Surfnet/StepupMiddleware/ApiBundle/Tests/Doctrine/Type/InstitutionRoleTypeTest.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Copyright 2016 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\ApiBundle\Tests\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit_Framework_TestCase as UnitTest;
+use Surfnet\Stepup\Configuration\Value\InstitutionRole;
+use Surfnet\Stepup\Identity\Value\Institution;
+use Surfnet\StepupMiddleware\ApiBundle\Doctrine\Type\InstitutionRoleType;
+
+class InstitutionRoleTypeTest extends UnitTest
+{
+    /**
+     * @var \Doctrine\DBAL\Platforms\MySqlPlatform
+     */
+    private $platform;
+
+    /**
+     * Register the type, since we're forced to use the factory method.
+     */
+    public static function setUpBeforeClass()
+    {
+        Type::addType(
+            InstitutionRoleType::NAME,
+            'Surfnet\StepupMiddleware\ApiBundle\Doctrine\Type\InstitutionRoleType'
+        );
+    }
+
+    public function setUp()
+    {
+        $this->platform = new MySqlPlatform();
+    }
+
+    /**
+     * @test
+     * @group doctrine
+     */
+    public function a_null_value_remains_null_in_to_sql_conversion()
+    {
+        $configurationInstitution = Type::getType(InstitutionRoleType::NAME);
+
+        $value = $configurationInstitution->convertToDatabaseValue(null, $this->platform);
+
+        $this->assertNull($value);
+    }
+
+    /**
+     * @test
+     * @group doctrine
+     */
+    public function a_non_null_value_is_converted_to_the_correct_format()
+    {
+        $configurationInstitution = Type::getType(InstitutionRoleType::NAME);
+
+        $expected = 'use_ra';
+        $input    = new InstitutionRole($expected);
+        $output   = $configurationInstitution->convertToDatabaseValue($input, $this->platform);
+
+        $this->assertTrue(is_string($output));
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * @test
+     * @group doctrine
+     */
+    public function a_null_value_remains_null_when_converting_from_db_to_php_value()
+    {
+        $configurationInstitution = Type::getType(InstitutionRoleType::NAME);
+
+        $value = $configurationInstitution->convertToPHPValue(null, $this->platform);
+
+        $this->assertNull($value);
+    }
+
+    /**
+     * @test
+     * @group doctrine
+     */
+    public function a_non_null_value_is_converted_to_a_configuration_institution_value_object()
+    {
+        $configurationInstitution = Type::getType(InstitutionRoleType::NAME);
+
+        $input = 'use_ra';
+
+        $output = $configurationInstitution->convertToPHPValue($input, $this->platform);
+
+        $this->assertInstanceOf('Surfnet\Stepup\Configuration\Value\InstitutionRole', $output);
+        $this->assertEquals(new InstitutionRole($input), $output);
+    }
+
+    /**
+     * @test
+     * @group doctrine
+     * @expectedException \Doctrine\DBAL\Types\ConversionException
+     */
+    public function an_invalid_database_value_causes_an_exception_upon_conversion()
+    {
+        $configurationInstitution = Type::getType(InstitutionRoleType::NAME);
+
+        $configurationInstitution->convertToPHPValue(false, $this->platform);
+    }
+}

037bdc1f7715f78c7773f7ea7140a7e997af36ce
Bas Strooband <bstrooband@ibuildings.nl>
2018-10-25 11:34:48 +0200

    Add id as first column in ra tables

--- app/DoctrineMigrations/Version20181023151546.php
+++ app/DoctrineMigrations/Version20181023151546.php
@@ -19,12 +19,12 @@ class Version20181023151546 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE ra_listing DROP PRIMARY KEY');
-        $this->addSql('ALTER TABLE ra_listing ADD id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, ADD ra_institution VARCHAR(255) NOT NULL COMMENT \'(DC2Type:institution)\'');
+        $this->addSql('ALTER TABLE ra_listing ADD id INT AUTO_INCREMENT PRIMARY KEY NOT NULL FIRST, ADD ra_institution VARCHAR(255) NOT NULL COMMENT \'(DC2Type:institution)\'');
         $this->addSql('CREATE UNIQUE INDEX idx_ra_listing_unique_identity_institution ON ra_listing (identity_id, ra_institution)');
         $this->addSql('ALTER TABLE ra_candidate DROP PRIMARY KEY');
         $this->addSql('ALTER TABLE ra_candidate ADD ra_institution VARCHAR(255) NOT NULL COMMENT \'(DC2Type:institution)\'');
         $this->addSql('CREATE UNIQUE INDEX idx_ra_candidate_unique_identity_institution ON ra_candidate (identity_id, ra_institution)');
-        $this->addSql('ALTER TABLE ra_candidate ADD id INT');
+        $this->addSql('ALTER TABLE ra_candidate ADD id INT FIRST');
         $this->addSql('ALTER TABLE ra_candidate ADD INDEX(`id`)');
         $this->addSql('ALTER TABLE ra_candidate CHANGE id id INT NOT NULL AUTO_INCREMENT');
         $this->addSql('ALTER TABLE ra_candidate ADD PRIMARY KEY (id)');

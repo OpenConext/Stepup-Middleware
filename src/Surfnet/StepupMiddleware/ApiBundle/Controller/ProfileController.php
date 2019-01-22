bc8903d0985bae2153d471365a23bc9344404f04
Michiel Kodde <mkodde@ibuildings.nl>
2019-01-22 13:25:07 +0100

    Tie the logic together hin the controller

---
+++ src/Surfnet/StepupMiddleware/ApiBundle/Controller/ProfileController.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\ApiBundle\Controller;
+
+use Surfnet\Stepup\Configuration\Value\InstitutionRole;
+use Surfnet\StepupMiddleware\ApiBundle\Authorization\Value\InstitutionRoleSet;
+use Surfnet\StepupMiddleware\ApiBundle\Identity\Service\ProfileService;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ProfileController extends Controller
+{
+    /**
+     * @var ProfileService
+     */
+    private $profileService;
+
+    /**
+     * @var InstitutionRoleSet
+     */
+    private $roleRequirements;
+
+    public function __construct(ProfileService $profileService)
+    {
+        $this->profileService = $profileService;
+        $this->roleRequirements = new InstitutionRoleSet(
+            [new InstitutionRole(InstitutionRole::ROLE_USE_RA), new InstitutionRole(InstitutionRole::ROLE_USE_RAA)]
+        );
+    }
+
+    public function getAction(Request $request, $identityId)
+    {
+        $this->denyAccessUnlessGranted(['ROLE_RA']);
+
+        // Is the actor allowed to view the profile page?
+        $actorId = $request->get('actorId');
+        if ($identityId !== $actorId) {
+            throw new AccessDeniedHttpException("Identity and actor id should match. It is not yet allowed to view the profile of somebody else.");
+        }
+
+        $profile = $this->profileService->createProfile($identityId);
+        if (!$profile) {
+            throw new NotFoundHttpException("The profile cannot be created, the identity id did not match an identity.");
+        }
+        return new JsonResponse($profile);
+    }
+}

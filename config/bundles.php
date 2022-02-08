317fb3feb6e18e58fd9737e8c2c59d165311aab0
Peter Havekes <peter@havekes.eu>
2022-02-08 10:28:12 +0100

    Update packages

--- config/bundles.php
+++ config/bundles.php
@@ -18,11 +18,11 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'smoketest' => true, 'dev_event_replay' => true, 'prod_event_replay' => true, 'smoketest_event_replay' => true],
     Surfnet\StepupMiddleware\ApiBundle\SurfnetStepupMiddlewareApiBundle::class => ['all' => true],
     Surfnet\StepupMiddleware\CommandHandlingBundle\SurfnetStepupMiddlewareCommandHandlingBundle::class => ['all' => true],
     Surfnet\StepupMiddleware\GatewayBundle\SurfnetStepupMiddlewareGatewayBundle::class => ['all' => true],
     Surfnet\StepupMiddleware\ManagementBundle\SurfnetStepupMiddlewareManagementBundle::class => ['all' => true],
     Surfnet\StepupMiddleware\MiddlewareBundle\SurfnetStepupMiddlewareMiddlewareBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];

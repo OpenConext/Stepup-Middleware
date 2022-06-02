03745dea05bee6636477c8dcdd9b4e88922c5720
Michiel Kodde <mkodde@ibuildings.nl>
2022-06-02 11:58:36 +0200

    Introduce Secret value objects
    These represent a secret in different scenarios. First an unhashed
    secret which can be turned into a hashed secret. Finally the secret can
    be forgotten.

---
+++ src/Surfnet/Stepup/Identity/Value/HashedSecret.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright 2022 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\Stepup\Identity\Value;
+
+/**
+ * The hashed secret, used on the safe-store recovery token type
+ */
+class HashedSecret implements Secret
+{
+    private $secret;
+
+    public function __construct(string $secret)
+    {
+        $this->secret = $secret;
+    }
+
+    public function getSecret(): string
+    {
+        return $this->secret;
+    }
+}

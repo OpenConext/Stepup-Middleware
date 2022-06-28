b7c9806841886a090a728758005b4e4ed72f2ae5
Michiel Kodde <mkodde@ibuildings.nl>
2022-06-28 13:31:41 +0200

    Upgrade Authorization::assertSatIsAllowed method
    Only when the Identity owned a self-asserted token (SAT), or when the
    identity does not have a tokey yet, allow the Identity to vet a self
    asesrted token.

--- src/Surfnet/StepupMiddleware/ApiBundle/Authorization/Service/AuthorizationService.php
+++ src/Surfnet/StepupMiddleware/ApiBundle/Authorization/Service/AuthorizationService.php
@@ -78,6 +78,16 @@ class AuthorizationService
             return $this->deny('Identity already has a vetted second factor');
         }
 
+        // Only allow self-asserted token (SAT) if the user does not have a token yet, or the first
+        // registered token was a SAT.
+        $options = $this->identityService->getSelfAssertedTokenRegistrationOptions(
+            $identity,
+            $this->secondFactorService->hasVettedByIdentity($identityId)
+        );
+        if ($options->possessedSelfAssertedToken === false) {
+            return $this->deny('Identity never possessed a self-asserted token, but did/does possess one of the other types');
+        }
+
         return $this->allow();
     }
 

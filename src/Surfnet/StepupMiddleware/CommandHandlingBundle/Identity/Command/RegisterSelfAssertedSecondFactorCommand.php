6a742d4bc9f8817997ff999d47f073a64da70035
Michiel Kodde <mkodde@ibuildings.nl>
2022-06-29 09:14:18 +0200

    Document RegisterSelfAssertedSecondFactorCommand
    As an alternative to documenting the commands in Markdown,
    we could also document them in PHP class doc-blocks. And use
    the field doc blocks when relevant.

--- src/Surfnet/StepupMiddleware/CommandHandlingBundle/Identity/Command/RegisterSelfAssertedSecondFactorCommand.php
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Identity/Command/RegisterSelfAssertedSecondFactorCommand.php
@@ -23,6 +23,43 @@ use Surfnet\StepupMiddleware\CommandHandlingBundle\Command\SelfAsserted;
 use Surfnet\StepupMiddleware\CommandHandlingBundle\Command\SelfServiceExecutable;
 use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * Register a self-asserted token
+ *
+ * A self-asserted token (SAT) registration registers a Second Factor token
+ * with the addition of passing a Recovery Token id which is used to
+ * verify the Identity is in possession of a Recovery Token.
+ *
+ * Registration of a SAT results in a SecondFactorVettedEvent (or the one
+ * without email verification). And sets the VettingType to be 'self-asserted'.
+ *
+ * Example body of HTTP Post request to Middleware API:
+ *
+ * ```
+ * {
+ *      "meta": {
+ *          "actor_id": "dea7e7dd-76ac-46f6-9ea9-a616bf8996a8",
+ *          "actor_institution": "institution-a.example.com"
+ *      },
+ *      "command": {
+ *          "name":"Identity:RegisterSelfAsseredtSecondFactor",
+ *          "uuid":"d12cb984-5720-405a-9533-af2beef78ee2",
+ *          "payload":{
+ *              "id": "e34cb336-5723-781a-9587-af2beef78aa2",
+ *              "identity_id": "dea7e7dd-76ac-46f6-9ea9-a616bf8996a8",
+ *              "second_factor_id": "0b88c646-9a73-43ea-a943-502cab15d5c2",
+ *              "second_factor_type": "yubikey",
+ *              "second_factor_identifier": "02513949",
+ *              "authoring_recovery_token_id": "1e598620-6cbb-4d14-b7f5-df0aaba847e5"
+ *          }
+ *      }
+ *  }
+ *
+ *
+ * Note: This is a SelfAsserted command. Marking it a token that is vetted without
+ * visiting a physical registration authority. Self-vetted tokens being the
+ * other 'self-asserted' token type.
+ */
 class RegisterSelfAssertedSecondFactorCommand extends AbstractCommand implements SelfServiceExecutable, SelfAsserted
 {
     /**
@@ -52,6 +89,11 @@ class RegisterSelfAssertedSecondFactorCommand extends AbstractCommand implements
     public $secondFactorType;
 
     /**
+     * The Identifier of the Second Factor Token. Examples:
+     * - Yubikey: this would be the public key,
+     * - SMS: the phone number
+     * - GSSP: The identifier the GSSP released during registration
+     *
      * @Assert\NotBlank()
      * @Assert\Type(type="string")
      *
@@ -60,6 +102,13 @@ class RegisterSelfAssertedSecondFactorCommand extends AbstractCommand implements
     public $secondFactorIdentifier;
 
     /**
+     * The ID of the Recovery Token used to authenticate the self-asserted token
+     * registration with. In case of a first time Topken registration, the Recovery
+     * Token was not authenticated. It's proof of possession was already proven by
+     * registering it. The Recovery Token id is passed along regardless
+     * registration type. And is used to project in the Audit Log what recovery token
+     * was used during registration.
+     *
      * @Assert\NotBlank()
      * @Assert\Type(type="string")
      *

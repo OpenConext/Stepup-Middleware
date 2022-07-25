d714a6a67233836ad343db0acb98c06ba5cdded0
Michiel Kodde <mkodde@ibuildings.nl>
2022-07-25 10:39:54 +0200

    Process VettingTypeHintCommand
    The vetting type hints are now processed and saved in the
    VettingTypeHintsSavedEvent. This event is not yet projected, but that's
    nex on the agenda.
    
    The hints themselves are saved in a collection that keys the translation
    locales with the hint texts that where received from the command body.
    The locales that are actually processed are the ones configured under
    the `locales` config in parameters.yaml. Take note that they should (but
    no must be) equal to those of the RA. If the RA app configures a
    different set of locales. Only the ones known to Middleware are
    processed.A warning is logged for each language that is lost that way.
    
    See: https://www.pivotaltracker.com/story/show/182129438

---
+++ src/Surfnet/StepupMiddleware/CommandHandlingBundle/Identity/Command/SaveVettingTypeHintCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright 2022 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddleware\CommandHandlingBundle\Identity\Command;
+
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Command\AbstractCommand;
+use Surfnet\StepupMiddleware\CommandHandlingBundle\Command\RaExecutable;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SaveVettingTypeHintCommand extends AbstractCommand implements RaExecutable
+{
+    /**
+     * The ID of an existing identity.
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type(type="string")
+     *
+     * @var string
+     */
+    public $identityId;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type(type="string")
+     *
+     * @var string
+     */
+    public $institution;
+
+    /**
+     * @Assert\Type(type="array")
+     * @Assert\All({
+     *      @Assert\NotBlank,
+     *      @Assert\Type("string")
+     * })
+     *
+     * @var string[]
+     */
+    public $hints;
+
+    public function getRaInstitution()
+    {
+        return null;
+    }
+}

357a694e7d07af5f7e3e04d01801cb073d0c0e81
Michiel Kodde <mkodde@ibuildings.nl>
2024-04-02 09:51:38 +0200

    Install and repair document headers

--- src/Surfnet/Stepup/Configuration/Value/SelfAssertedTokensOption.php
+++ src/Surfnet/Stepup/Configuration/Value/SelfAssertedTokensOption.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2022 SURF B.V.
+ * Copyright 2022 SURFnet B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
